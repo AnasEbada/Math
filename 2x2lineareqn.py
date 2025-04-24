ae43c5c4457302bb77d531800eb467e9a926a71e
AnasEbada <drmatef100@hotmail.com>
2025-04-24 15:04:59 +0200

    first points of solution

--- 2x2lineareqn.py
+++ 2x2lineareqn.py
@@ -1,6 +1,7 @@
 # this is supposted to be a python code to solove the 2 set linear equation 
 # side project
 import sys
+
 def take_input():
     print('\nEnter the coefficients for the system:')
     print('  a1*x + b1*y = c1')
@@ -73,8 +74,6 @@ def take_input():
 
         return fa1, fb1, fd1, fa2, fb2, fd2
 
-a1, b1, d1, a2, b2, d2 = take_input()
-
 def check_if_solvable(ca1, cb1, cd1, ca2, cb2, cd2):
     if ca1 == 0 or cb1 == 0 or cd1 == 0 or ca2 == 0 or cb2 == 0 or cd2 == 0:
         print('One or more coefficients are zero, which makes the system unsolvable.')
@@ -95,11 +94,19 @@ def check_if_solvable(ca1, cb1, cd1, ca2, cb2, cd2):
 def solve(sa1, sb1, sd1, sa2, sb2, sd2):
     # sa1X + sb1Y = Sd1
     # sa2X + sb2Y = sd2
+    mulnum = None
+    if sa1 > sa2: 
+        mulnum = -1 * sa1 / sa2
+        sa2, sb2, sd2 = mulnum * sa2, mulnum * sb2, mulnum * sd2
+    if sa1 < sa2:
+        mulnum = -1 * sa2 / sa1
+        sa1, sb1, sd1 = mulnum * sa1, mulnum * sb1, mulnum * sd1
+    
     return
 
 
 
-
+a1, b1, d1, a2, b2, d2 = take_input()
 
 
 
