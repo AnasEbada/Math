f1125f98b3ec8c05ed7eb8d9a6f1a8d69c0f00e9
AnasEbada <drmatef100@hotmail.com>
2025-05-29 13:41:42 +0300

    progress

--- rationalfunc.py
+++ rationalfunc.py
@@ -1,50 +1,40 @@
 import re
 
-# code structure 
 # *  f(x) = (x2 + x - 2) / (2x2 - 2x - 3) 
-'''
-1. we take the values from the user as q(x) and p(x) #! getvalues()
-** make sure that the values are valid #! getvalues()
-2. we split the terms over some list #! dicts()
-3. we already have dictionary that has up to 10 keys #! sfw()
-4. done, now we have dect for the n and the d, now we take the d and we get the zeros from it and done #! domain()
-** we will do that for both n and d 
-and lastly the main script that the code and all functions will be called in and it will be #! mainscript()
-'''
+
 def getvalues():
     validinputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'x', '+', '-', '^']
+    essentialinputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'x']
     while True:
-        px = input('Please Enter P(x): ')
+        px = input('Please Enter P(x): ').replace(" ", "")
         ispxvalid = True
         for i in px:
             if i not in validinputs:
                 print('Invalid Input: Please Try Again')
                 ispxvalid = False
-            else:
                 break
         if ispxvalid:
             break
-                
+
     while True:
-        qx = input('Please Enter Q(x): ')
+        qx = input('Please Enter Q(x): ').replace(" ", "")
         isqxvalid = True
-        for i in px:
+        for i in qx:
             if i not in validinputs:
                 print('Invalid Input: Please Try Again')
                 isqxvalid = False
-            else:
                 break
         if isqxvalid:
             break
     return px, qx
 
-def listterms(px: str, qx: str):
-    # pointing pc and qx as strings so vscode is able to use #! replace()
-    px_nospaces = px.replace(" ", "")
-    pxlist = re.split(r"[+-]", px_nospaces)
-    qx_nospaces = qx.replace(" ", "")
-    qxlist = re.split(r"[+-]", qx_nospaces)
-    return pxlist, qxlist
+def zeros(px: str, qx: str):
+    #! 2x^2
+    px.replace("^", "**")
+    qx.replace("^", "**")
+    px.replace("x", "")
+    return
+
 
 
     
