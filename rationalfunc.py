aa25bef4c9ceaf4275d894c7ea19ade6b1228a92
AnasEbada <drmatef100@hotmail.com>
2025-05-29 14:03:38 +0300

    paused

--- rationalfunc.py
+++ rationalfunc.py
@@ -3,38 +3,43 @@ import re
 # *  f(x) = (x2 + x - 2) / (2x2 - 2x - 3) 
 
 def getvalues():
-    validinputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'x', '+', '-', '^']
-    essentialinputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'x']
+    '''
+    validinputs = ['1','2','3','4','5','6','7','8','9','0','x','+','-','^']
+    essentialinputs = ['1','2','3','4','5','6','7','8','9','0','x']
+
+    def is_valid(poly):
+        for char in poly:
+            if char not in validinputs:
+                print('Invalid Input: Contains invalid characters. Please Try Again.')
+                return False
+    #?  if not any(char in essentialinputs for char in poly):
+            print('Invalid Input: Must contain at least one number or variable (x).')
+            return False
+        return True
+
     while True:
         px = input('Please Enter P(x): ').replace(" ", "")
-        ispxvalid = True
-        for i in px:
-            if i not in validinputs:
-                print('Invalid Input: Please Try Again')
-                ispxvalid = False
-                break
-        if ispxvalid:
+        if is_valid(px):
             break
 
     while True:
         qx = input('Please Enter Q(x): ').replace(" ", "")
-        isqxvalid = True
-        for i in qx:
-            if i not in validinputs:
-                print('Invalid Input: Please Try Again')
-                isqxvalid = False
-                break
-        if isqxvalid:
+        if is_valid(qx):
             break
+
     return px, qx
+    '''
 
 def zeros(px: str, qx: str):
-    #! 2x^2
+    # 2x^2
+    '''
     px.replace("^", "**")
     qx.replace("^", "**")
-    px.replace("x", "")
+    #? px.replace("x", "")
+    '''
     return
 
+#TODO we are going to study this, but not now. pasued project.
 
 
     
